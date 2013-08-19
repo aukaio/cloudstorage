95588687e80d496e2d88c4b547ea2fe6760489c5
Espen Stene Brønstad <esb@mcash.no>
2013-08-19 16:09:57 +0200

    Moved py files into cloudstorage subdir and added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='cloudstorage',
+    version='0.1mcash',
+    author="Google",
+    description="Cloudstorage api",
+    packages=['cloudstorage'],
+    include_package_data=True,
+    install_requires=[
+    ],
+)
