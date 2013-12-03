96688d1e2eb72982a56986b03c5e31df9bba65f8
Dag Sverre Seljebotn <d.s.seljebotn@astro.uio.no>
2013-12-03 10:47:39 +0100

    Update to r127

--- setup.py
+++ setup.py
@@ -1,13 +1,28 @@
-from setuptools import setup, find_packages
+"""Setup specs for packaging, distributing, and installing gcs lib."""
+
+import distribute_setup
+distribute_setup.use_setuptools()
 
 
-setup(
-    name='cloudstorage',
-    version='0.1mcash',
-    author="Google",
-    description="Cloudstorage api",
-    packages=['cloudstorage'],
-    include_package_data=True,
-    install_requires=[
-    ],
+import setuptools
+
+
+setuptools.setup(
+    name="GoogleAppEngineCloudStorageClient",
+    version="1.8.3.1",
+    packages=setuptools.find_packages(),
+    author="Google App Engine",
+    author_email="app-engine-pipeline-api@googlegroups.com",
+    keywords="google app engine cloud storage",
+    url="https://code.google.com/p/appengine-gcs-client/",
+    license="Apache License 2.0",
+    description=("This library is the preferred way of accessing Google "
+                 "Cloud Storage from App Engine. It was designed to "
+                 "replace the Files API. As a result it contains much "
+                 "of the same functionality (streaming reads and writes but "
+                 "not the complete set of GCS APIs). It also provides key "
+                 "stability improvements and a better overall developer "
+                 "experience."),
+    exclude_package_data={"": ["README"]},
+    zip_safe=True,
 )
